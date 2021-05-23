dc251dfd864493a1635cc04b2014056663ce5000
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-05-23 13:35:19 +0100

    added-incorrect-answers-to-the-page

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const displaysSection = document.querySelector(".question-answer")
 const typeOfQuestion = "multiple" // boolean / Every Boolean answer is true can't use this for a true or false API
 const difficultyLevel = "easy"; // medium || hard - can hard code it for now
 const musicButton = document.getElementById("music-category")
-let categorynumber = null;
+
 
 
 // testing API data  - Open trivia data base API 
@@ -20,11 +20,11 @@ async function fetchQuizQuestions(){
     // use fetch to do GET request for questions
     // chose music category; amount of questions = 10; difficulty =easy; type = multiple( multiple choice)
     // you can change the amount, category,difficulty, and type based on user input
-    const requestUrl = await fetch (`https://opentdb.com/api.php?amount=10&category=${categorynumber}&difficulty=${difficultyLevel}&type=${typeOfQuestion}`);
+    const requestUrl = await fetch (`https://opentdb.com/api.php?amount=10&category=12&difficulty=${difficultyLevel}&type=${typeOfQuestion}`);
     const dataResponse = await requestUrl.json();
     console.log(dataResponse);
     const result = dataResponse.results;
-      generateQuestions(result);
+    generateQuestions(result);
       
 }
 
@@ -35,21 +35,20 @@ fetchQuizQuestions();
       let generateQuestions = "";
       results.map(result => {
         generateQuestions  += 
-        `<div class="border question-background"> ${result.question
-        }</div>
-        <div class="border answer-background">${result.correct_answer}</div>`
+        `
+        <div class="border question-background"> ${result.question}</div>
+        <div class="border answer-background">${result.correct_answer} ✔ </div>
+        <div class="border answer-background">${result.incorrect_answers[0]}</div>
+        <div class="border answer-background">${result.incorrect_answers[1]}</div>
+        <div class="border answer-background">${result.incorrect_answers[2]}</div>
+
+        `
       })
       displaysSection.innerHTML = generateQuestions;
     }
  
-    function displayMusicQuestion(categorynumber){
-      onclick="document.location='q+a-page.html'"
-      displayQuestionAndOneAnswer(categorynumber)
-     }
-
-musicButton.addEventListener("click", displayMusicQuestion(12))
+ 
+    
 
   
    
-
-fetchQuizQuestions();
